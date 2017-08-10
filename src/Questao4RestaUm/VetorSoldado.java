f82f81a11c73b89ef315b3797ab27b3c63e71ee3
DESKTOP-U973K82\Barbara <barbara.marquez@outlook.com.br>
2017-08-10 20:59:34 -0300

    Alterações no array de soldados

--- src/Questao4RestaUm/VetorSoldado.java
+++ src/Questao4RestaUm/VetorSoldado.java
@@ -5,78 +5,106 @@
  */
 package Questao4RestaUm;
 
-import static javafx.scene.input.KeyCode.T;
+import java.util.Random;
 
 /**
  *
  * @author Barbara
  */
 public class VetorSoldado {
-    private Object[] info;
+    private final int INTERVALO_DE_NUMEROS_INICIO = -9;
+    private final int INTERVALO_DE_NUMEROS_FIM = 9;
+    
+    private Soldado[] info;
     private int limite;
     private int tamanho;
+    private int posicaoAtual;
 
-    public VetorSoldado(int limite)
-    {
-        this.info = new Object[limite];
+    public VetorSoldado(int limite) {
+        this.info = new Soldado[limite];
         this.limite = limite;
         this.tamanho = 0;
     }
 
-    public Object[] getInfo()
-    {
+    public Object[] getInfo() {
         return info;
     }
 
-    public void setInfo(Object[] info)
-    {
+    public void setInfo(Soldado[] info) {
         this.info = info;
     }
 
-    public int getLimite()
-    {
+    public int getLimite() {
         return limite;
     }
 
-    public void setLimite(int limite)
-    {
+    public void setLimite(int limite) {
         this.limite = limite;
     }
 
-    public int getTamanho()
-    {
+    public int getTamanho() {
         return tamanho;
     }
 
-    public void setTamanho(int tamanho)
-    {
+    public void setTamanho(int tamanho) {
         this.tamanho = tamanho;
-    }           
+    }
 
-    public boolean estaVazio()
-    {
+    public boolean estaVazio() {
         return tamanho == 0;
     }
-    
-    
 
-    public void liberar()
-    {
-        Object[] novo = new Object[limite];
+    public void remover(int posicao) {
+        //Deve conseguir remover o valor da posição
+        if (posicao == tamanho) {
+            info[posicao] = null;
+        } else {
+            for (int i = posicao; i < tamanho; i++) {
+                info[i] = info[i + 1];
+            }
+            info[tamanho] = null;
+        }
+
+        tamanho--;
+    }
+
+    public void liberar() {
+        Soldado[] novo = new Soldado[limite];
         this.info = novo;
         this.tamanho = 0;
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         String result = "";
-        for (int i = tamanho-1; i >= 0; i--) {
+        for (int i = tamanho - 1; i >= 0; i--) {
             result += info[i] + ",";
         }
-        
-        result = (result.charAt(result.length()-1) == ',') ? result.substring(0, result.length()-1) : result;
-        
+
+        result = (result.charAt(result.length() - 1) == ',') ? result.substring(0, result.length() - 1) : result;
+
         return result;
+    }
+
+    public int NumeroSorteado() {
+        Random random = new Random(INTERVALO_DE_NUMEROS_INICIO);
+        int numero = random.nextInt(INTERVALO_DE_NUMEROS_FIM);
+        
+        return numero;
+    }
+
+    public int RetornarPosicaoRemover() {
+        return 0;
+    }
+
+    public Soldado restaUm() {
+        //Sorteia um soldado
+        posicaoAtual = NumeroSorteado();
+        //Enquanto tiver mais de um
+        //Sorteia um número
+        //Contabiliza esse número
+        //Remove o cara
+        //Retorna o vencedor
+        return info[0];
     }
 }
